80daddccb4c2dcc8968b118d2258ef346d68b45b
Wizzy <wizzypatrick777@gmail.com>
2024-09-19 21:53:33 +0100

    First commit

---
+++ src/main/java/org/example/nightclass/TodaysNightClass.java
@@ -0,0 +1,15 @@
+package org.example.nightclass;
+
+public class TodaysNightClass {
+    public static void main(String[] args) {
+
+        //print hello world
+
+        System.out.println("Hello World");
+
+    }
+
+
+
+
+}
